bba51dc75a3ae29744d6958484c5507d991859c4
Nononox <taor@umich.edu>
2017-12-30 23:07:24 -0500

    Delete plot3.R

--- plot3.R
+++
@@ -1,8 +0,0 @@
-data <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
-usefulData <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
-D = as.Date(usefulData$Date, format = "%d/%m/%Y")
-DANDT <- as.POSIXct(paste(D, usefulData$Time), format="%Y-%m-%d %H:%M:%S")
-with(usefulData, plot(DANDT, as.numeric(levels(usefulData$Sub_metering_1))[usefulData$Sub_metering_1], type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
-lines(DANDT, as.numeric(levels(usefulData$Sub_metering_2))[usefulData$Sub_metering_2], type = "l", col = "red")
-lines(DANDT, usefulData$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), lwd=c(2.5, 2.5, 2.5), col = c("black", "red", "blue"))
